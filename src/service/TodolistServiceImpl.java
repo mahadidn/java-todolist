7c788bc1cd183c8ce168db35d6d7c44322c353f0
Mahadi Dwi Nugraha <mahadidwinugraha@gmail.com>
2024-03-06 16:49:25 +0700

    add todolistService implementation

---
+++ src/service/TodolistServiceImpl.java
@@ -0,0 +1,38 @@
+package service;
+
+import entity.Todolist;
+import repository.TodolistRepository;
+
+public class TodolistServiceImpl implements TodolistService {
+
+    private TodolistRepository todolistRepository;
+
+    public TodolistServiceImpl(TodolistRepository todolistRepository) {
+        this.todolistRepository = todolistRepository;
+    }
+
+    @Override
+    public void showTodolist() {
+        Todolist[] model = todolistRepository.getAll();
+
+        System.out.println("TODOLIST:");
+        for (var i = 0; i < model.length; i++){
+            var todo = model[i];
+            var no = i + 1;
+
+            if(todo != null){
+                System.out.println(no + ". " + todo);
+            }
+        }
+    }
+
+    @Override
+    public void addTodolist(String todo) {
+
+    }
+
+    @Override
+    public void removeTodolist(Integer number) {
+
+    }
+}
